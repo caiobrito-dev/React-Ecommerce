59c290e50995f41a83331010f66ad0aa834c6b5f
Caio Barcelos de Brito <caiobarcelosdebrito@gmail.com>
2025-05-23 23:24:38 -0300

    Starting header functions

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import { IoMdSearch } from "react-icons/io";    
 
 import "./SearchBar.css"
 
 function SearchBar () {
+    const [searchValue, setValue] = useState("")
     return (
         <form className="SearchBar">
-            <input type="search" placeholder="Buscar produto" className="SearchInput" required/>
+            <input type="search" 
+            placeholder="Buscar produto" 
+            className="SearchInput" required 
+            onChange={ ( {target} ) => setValue(target.value)} 
+            value={searchValue} />
+
             <button type="submit" className="SearchButton"><IoMdSearch /></button>
         </form>
     );
